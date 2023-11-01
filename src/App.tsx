0834bba545ed7fb03706d67c46649c69ad02ffd9
Hugh Morrison <hmorri32@gmail.com>
2023-11-01 12:27:28 -0400

    scaffolds app layout and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import { Home } from './components/Home/Home';
+import { JokeDetail } from './JokeDetail';
+import { JokeOfTheDay } from './JokeOfTheDay';
+import { RandomJoke } from './components/RandomJoke/RandomJoke';
+import { Search } from './components/Search/Search';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/search">Search</Link>
+          <Link to="/random">Random Joke</Link>
+          <Link to="/joke-of-the-day">Joke of the Day</Link>
+        </nav>
+      </header>
+
+      <main style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/random" element={<RandomJoke />} />
+          <Route path="/joke-of-the-day" element={<JokeOfTheDay />} />
+          <Route path="/joke/:id" element={<JokeDetail />} />
+        </Routes>
+      </main>
+
+      <footer className="jokes-footer">
+        <p>© Can I Haz Dad Joke?</p>
+      </footer>
+    </Router>
+  );
 }
 
-export default App
+export default App;
